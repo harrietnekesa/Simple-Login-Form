ec20f76cb94bbc75036ae6865e8d2dbc7136edad
Nekesa <87010562+harrietnekesa@users.noreply.github.com>
2023-10-09 16:42:48 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+	<div class="container2">
+
+		<div class=cont1>
+			<img src="images/Screenshot 2023-09-15 000605.png">
+
+		</div>
+
+<div class="cont2">
+	<h1>LOG IN</h1>
+	
+	<form method="POST">
+
+		PHONE NUMBER:<input type="number" name="number"><p>
+		PASSWORD :<input type="password" name="password">
+
+	</form>
+
+	<h2>To create an account!<a href="signup.php">SIGN UP</a></h2>
+</div>
+
+</div>
+
+
+</body>
+</html>
